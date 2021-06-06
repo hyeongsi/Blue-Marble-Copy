11e200eae532ecc9cefc88d1693d7a49a9d0d990
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2021-06-07 06:56:04 +0900

    랜덤 주사위, 초기자금 설정
    1. 랜덤 주사위, 초기자금 설정
    - 디버깅을 위해, 높은 값의 랜덤주사위, 낮은 초기자금으로 세팅되어 있었으나, 모두 구현해 필요성이 없어져 정상적인 수치로 변경했습니다.

--- BlueMarble_Server/commonGameResource.h
+++ BlueMarble_Server/commonGameResource.h
@@ -1,6 +1,6 @@
 #pragma once
 
 constexpr const int SALARY = 300;
-constexpr const int START_MONEY = 100;
+constexpr const int START_MONEY = 2000;
 constexpr const int TAX = 200;
 constexpr const int ESCAPE_COUNT = 1;
